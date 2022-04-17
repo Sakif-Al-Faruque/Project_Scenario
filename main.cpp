346f440192f728c0e063ef53886ee9ebadaa0409
Sakif-Al-Faruque <sakif.saf915@gmail.com>
2022-04-17 22:42:50 +0600

    main branch has been updated

--- main.cpp
+++ main.cpp
@@ -7,19 +7,44 @@
 
 using namespace std;
 
-int main()
+//main branch is started
+void init(void){
+    glClearColor(0,0,0,0); //GLfloat red,green,blue,alpha initial value 0 alpha values used by glclear to clear the color buffers
+    glMatrixMode(GL_PROJECTION);  // To specify which matrix is the current matrix & projection applies subsequent matrix to projecton matrix stack
+    glLoadIdentity(); // Load Matrix
+    gluOrtho2D(0,1000,600,0); //set total 4 quadrant(GLdouble left, GLdouble right, GLdouble bottom, GLdouble top)
+
+}
+//main branch is ended
+
+void Draw()
 {
-   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glClear(GL_COLOR_BUFFER_BIT); //main
+
+
+
+    glPushMatrix();//main
+
+	glPopMatrix();//main
+
+    glFlush();//main
+}
+
+//main branch is started
+int main(){
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(1000,600);
     glutInitWindowPosition(0, 0);
-    glutCreateWindow("Scenario");
+    glutCreateWindow("Project Scenario");
 
     init();
 
-    glutDisplayFunc();
+    glutDisplayFunc(Draw);
+
     glutMainLoop();
     return 0;
 }
+//main branch is ended
 
 
 
